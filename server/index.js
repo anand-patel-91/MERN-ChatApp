fa87e58a917ad9c6d4a4194bf14d3dd066bd1cfa
Anand Patel <patel91a@gmail.com>
2024-06-06 23:07:42 +0530

    env var ka nam-o-nishan mita diya

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
-require("dotenv").config();
 const messageRoutes = require("./routes/messages");
 const userRoutes = require("./routes/user");
 const userChatRoutes = require("./routes/userChats");
--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -11,7 +11,7 @@ const requireAuth = async (req, res, next) => {
   const token = authorization.split(" ")[1];
 
   try {
-    const { _id } = jwt.verify(token, process.env.REACT_APP_SECRET);
+    const { _id } = jwt.verify(token, "anandpatelsecretkey");
 
     req.user = await User.findOne({ _id }).select("_id");
     next();
