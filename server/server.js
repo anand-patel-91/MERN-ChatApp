5b8abb1f47e517b3dc4994230ee9c5fb69b914c6
Anand Patel <patel91a@gmail.com>
2024-06-06 22:37:28 +0530

    testing

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.use("/api/userChats", userChatRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT || 4000, () => {
-      console.log("connected to db and listening on port", process.env.PORT || 4000);
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
     });
   })
   .catch((error) => {
