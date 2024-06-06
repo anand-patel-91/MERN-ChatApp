f969897c9eea9e2a3b07702713c3fdcc792ddd09
Anand Patel <patel91a@gmail.com>
2024-06-06 21:38:42 +0530

    testing

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const userChatRoutes = require("./routes/userChats");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use('/', (req, res)=> {
     res.send("Server is running");
@@ -23,7 +23,7 @@ app.use("/api/userChats", userChatRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 4000, () => {
       console.log("connected to db and listening on port", process.env.PORT);
     });
   })
